919e608388cd47405c4f1176d86b34b7d77f6213
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2024-06-07 15:38:28 +0530

    feat:map topodata

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,34 +1,38 @@
-import React, { useEffect, useRef } from 'react';
-import L from 'leaflet';
-import data from './data.json'; // Import your JSON data
-
-import 'leaflet/dist/leaflet.css';
-
+import React, { useState, useEffect, useRef } from "react";
+import data from "../data/data.json"; // Import your JSON data
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+const geoUrl =
+  "https://raw.githubusercontent.com/shuklaneerajdev/IndiaStateTopojsonFiles/master/Chhattisgarh.topojson";
 const Map = () => {
-  const mapRef = useRef();
 
-  useEffect(() => {
-    const map = L.map(mapRef.current).setView([20.5937, 82.9629], 7);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      maxZoom: 18,
-    }).addTo(map);
-
-    // Add GeoJSON layer
-    const geojsonLayer = L.geoJSON(data, {
-      onEachFeature: (feature, layer) => {
-        const { district, schools, teachers, students } = feature.properties;
-        layer.bindPopup(`<strong>${district}</strong><br/> Schools: ${schools}<br/> Teachers: ${teachers}<br/> Students: ${students}`);
-      },
-    }).addTo(map);
-
-    return () => {
-      map.remove();
-    };
-  }, []);
-
-  return <div ref={mapRef} style={{ height: '600px', width: '100%' }} />;
+  return (
+    <div>
+      <ComposableMap>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                style={{
+                  default: {
+                    fill: "#EEE",
+                  },
+                  hover: {
+                    fill: "#F53",
+                  },
+                  pressed: {
+                    fill: "#E42",
+                  },
+                }}
+              />
+            ))
+          }
+        </Geographies>
+      </ComposableMap>
+    </div>
+  );
 };
 
 export default Map;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import { Map } from "./Map";
+import Map  from "./Map";
 
 export { Footer, Navbar,Map };
