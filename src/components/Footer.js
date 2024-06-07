9ce4ac2fa54de1d3ed7218673f4c7d16a86e92cf
vinayak180203 <vinayakk@iitbhilai.ac.in>
2024-06-07 15:28:55 +0530

    Added navbar and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { FaTwitter, FaInstagram, FaFacebook, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-orange-400 p-8 mt-8 min-h-24 flex items-center absolute bottom-0 w-full">
+      <div className="container mx-auto flex justify-center">
+        <div className="flex space-x-4">
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-gray-800 text-2xl">
+            <FaTwitter />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="text-gray-800 text-2xl">
+            <FaInstagram />
+          </a>
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-gray-800 text-2xl">
+            <FaFacebook />
+          </a>
+          <a href="https://youtube.com" target="_blank" rel="noopener noreferrer" className="text-gray-800 text-2xl">
+            <FaYoutube />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
