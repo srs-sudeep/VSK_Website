696215d9fb0f068962a257aa3967156768aaffde
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2024-06-07 13:47:31 +0530

    feat:routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import LoadingPage from "./components/LoadingPage";
+// import Navbar from "./components/Navbar";/
+// import Footer from "./components/Footer";
+import { Navbar,Footer } from "./components";
+import { Home } from "./pages";
 function App() {
+  // const [loading, setLoading] = useState(true);
+  // const location = useLocation();
+  // const [vantaEffect, setVantaEffect] = useState(null);
+  // const myRef = useRef(null);
+  // useEffect(() => {
+  //   if (!vantaEffect) {
+  //     setVantaEffect(
+  //       GLOBE({
+  //         el: myRef.current,
+  //         mouseControls: true,
+  //         touchControls: true,
+  //         gyroControls: true,
+  //         minHeight: 20.0,
+  //         minWidth: 20.0,
+  //         scale: 0.05,
+  //         scaleMobile: 1.0,
+  //         size: 0.5,
+  //       })
+  //     );
+  //   }
+  //   return () => {
+  //     if (vantaEffect) vantaEffect.destroy();
+  //   };
+  // }, [vantaEffect]);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000);
+  // }, []);
+
+  // if (loading) {
+  //   return <LoadingPage />;
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <div className="overlay">
+        <div ref={myRef} className="bg" id="vanta">
+          {" "}
+        </div>
+      </div> */}
+      <Navbar />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+// homepage , projects ,project,blog, community,programs
