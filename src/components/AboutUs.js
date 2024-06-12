03777e368a6541447e68e3e55243d26971e0c7d9
vinayak180203 <vinayakk@iitbhilai.ac.in>
2024-06-12 19:34:04 +0530

    Made About Us responsive

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,16 +4,16 @@ import about_us_image from '../data/samagra siksha logo.png';
 
 const AboutUsSection = () => {
   return (
-    <section id="about-us" className="relative h-screen bg-cover bg-center" style={styles.container}>
+    <section id="about-us" className="relative bg-cover bg-center flex items-center justify-center" style={styles.container}>
         {/* <div className="absolute inset-0 bg-gray-800 opacity-90"></div> */}
-      <div className="absolute inset-0 flex items-center justify-center">
-        <div className="bg-white shadow-lg w-[79%] flex-row rounded-lg p-10 w-full mx-4" style={styles.card}>
+      {/* <div className="absolute inset-0 flex items-center justify-center"> */}
+        <div className="bg-white shadow-lg rounded-lg md:w-[75vw] w-[100vw] p-10 flex flex-col items-center justify-center" style={styles.card}>
         <h1 className="text-3xl font-bold mt-4 mb-4 text-center texthead leading-relaxed font-[600]"><span className='bg-orange-400 px-1 py-1 bg-opacity rounded-md text-[#fff]'>About</span> Us</h1>
-          <div className="flex px-20 md:px-8 lg:px-16 mt-16 items-center" >
-            <div className="w-1/3 pr-16 flex-col">
+          <div className="flex flex-col lg:flex-row px-20 md:px-8 lg:px-16 mt-16 items-center" >
+            <div className="w-1/3 pr-16">
               <img src={about_us_image} alt="About Us" className="rounded-lg w-full h-full object-cover" />
             </div>
-            <div className="w-2/3 flex mt-1 pr-10 flex-col">
+            <div className="w-2/3 flex mt-1 pr-10">
               <div>
                 <p className="text-[22px] font-sora">
                 At Vidya Samiksha Kendra, our mission is to significantly enhance the academic performance of students while empowering teachers through innovative educational strategies and the optimal use of available resources. By fostering an environment where learning thrives, we are dedicated to unlocking the full potential of every student.                </p>
@@ -23,7 +23,7 @@ const AboutUsSection = () => {
             </div>
           </div>
         </div>
-      </div>
+      {/* </div> */}
     </section>
   );
 }
