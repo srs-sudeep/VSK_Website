879046f50cc567b0665b3417e6e5e9bb7e0d92aa
rahul1416 <rahuluino@gmail.com>
2024-06-12 20:06:04 +0530

    added animation v2

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import background_image from '../data/background_about_us.jpeg';
 import about_us_image from '../data/samagra siksha logo.png';
-
+import "animate.css";
 const AboutUsSection = () => {
   return (
     <section id="about-us" className="relative bg-cover bg-center flex items-center justify-center" style={styles.container}>
         {/* <div className="absolute inset-0 bg-gray-800 opacity-90"></div> */}
       {/* <div className="absolute inset-0 flex items-center justify-center"> */}
-        <div className="bg-white shadow-lg rounded-lg p-10 my-5 flex flex-col items-center justify-center w-[100vw] md:w-[75vw]" style={styles.card}>
+        <div className="bg-white shadow-lg rounded-lg p-10 my-5 flex flex-col items-center justify-center w-[100vw] md:w-[75vw] animate__animated animate__fadeInLeft" style={styles.card}>
         <h1 className="text-3xl font-bold mt-4 mb-4 text-center texthead leading-relaxed font-[600]"><span className='bg-orange-400 px-1 py-1 bg-opacity rounded-md text-[#fff]'>About</span> Us</h1>
           <div className="flex flex-col lg:flex-row md:px-8 lg:px-16 mt-16 items-center" >
             <div className="lg:w-1/3 lg:pr-16">
